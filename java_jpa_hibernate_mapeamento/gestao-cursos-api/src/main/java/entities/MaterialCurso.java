1ed94a2e3cdc94f857e38b9a04100d83f1454e1b
Jean Oliveira <jean.valid@gmail.com>
2025-10-07 16:12:55 -0300

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/MaterialCurso.java
@@ -0,0 +1,12 @@
+package entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MaterialCurso {
+    private Long id;
+    private String url;
+}
+
---
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Telefone.java
@@ -0,0 +1,16 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Telefone {
+    private Long id;
+    private String DDD;
+    private String numero;
+}
